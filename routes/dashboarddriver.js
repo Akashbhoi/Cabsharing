a72b07dcd23ed0d33b3daf7bb28544d8591303d3
Rmukati1 <rishmuka@iitk.ac.in>
2022-03-27 14:09:10 +0530

    frontend

--- routes/dashboarddriver.js
+++ routes/dashboarddriver.js
@@ -30,6 +30,8 @@ router.get("/", function (req, res) {
           Noof: journeyData.Noof,
           origin: journeyData.origin,
           destination: journeyData.destination,
+          Departuredate: journeyData.Departuredate,
+          time: journeyData.time,
           accept: journeyData.accept,
           pending: driver.pending,        
         });
